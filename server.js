fe7e5762950d28da89be767d71e039a452a903a0
Dream <mac@MacBook-Pro.local>
2025-10-29 01:02:35 +0800

    fix code

--- server.js
+++ server.js
@@ -16,25 +16,31 @@ wss.on("connection", (ws) => {
 });
 
 async function openPageAndToUrl(url) {
-  console.log(`准备打开 ${url}`);
-  const browser = await puppeteer.launch({
-    headless: "new",
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-accelerated-2d-canvas",
-      "--disable-gpu",
-      "--window-size=1920,1080",
-    ],
-  });
-  const page = await browser.newPage();
-  await page.setUserAgent({
-    userAgent:
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
-  });
-  await page.goto(url, { waitUntil: "networkidle2" });
-  return { browser, page };
+  try {
+    console.log(`准备打开 ${url}`);
+    const browser = await puppeteer.launch({
+      headless: "new",
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-accelerated-2d-canvas",
+        "--disable-gpu",
+        "--window-size=1920,1080",
+      ],
+    });
+    const page = await browser.newPage();
+    await page.setUserAgent({
+      userAgent:
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
+    });
+    await page.goto(url, { waitUntil: "networkidle2" });
+    return { browser, page };
+  } catch {
+    console.log(`创建页面失败，重新创建 ${url}`)
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    return await openPageAndToUrl(url)
+  }
 }
 
 async function wwwAppBitagentIoPage() {
