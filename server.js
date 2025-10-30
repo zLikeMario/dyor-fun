a4ac7e9f12ab522402f44991ffb97e94fc33125f
Dream <mac@MacBook-Pro.local>
2025-10-30 21:05:11 +0800

    code

--- server.js
+++ server.js
@@ -69,65 +69,9 @@ async function b402scanAi() {
   // browser.close(); // 永远不会到达这里，因为无限循环
 }
 
-async function fourMemePage() {
-  const url = "https://four.meme/zh-TW/create-token?entry=fair-mode";
-  const { page } = await openPageAndToUrl(url);
-  while (true) {
-    try {
-      if (wsClients.length) {
-        await page.reload({ waitUntil: "networkidle2" });
-        console.log(`${url} 页面加载完成，开始抓取内容`);
-        await page.waitForSelector('button[id*="headlessui-listbox-button"]');
-        page.removeExposedFunction("onMutation").catch(() => { });
-        // 暴露一个 Node 端函数，让页面内的 observer 能调用
-        await page.exposeFunction("onMutation", (fourMeme) => {
-          console.log(`检测到新增元素：${fourMeme.length} 个`);
-          if (!fourMeme.length) return;
-          // 通过 ws 发送数据到所有客户端
-          wsClients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-              client.send(JSON.stringify({ fourMeme }));
-            }
-          });
-        });
-
-        // 在页面上下文中注册 MutationObserver
-        await page.evaluate(() => {
-          const observer = new MutationObserver((mutations) => {
-            const added = new Set();
-            for (const m of mutations) {
-              m.addedNodes.forEach((n) => {
-                if (n.nodeType === 1) {
-                  added.add(n.innerText);
-                }
-              });
-            }
-            if (added.size) window.onMutation(added);
-          });
-          observer.observe(document.body, { childList: true, subtree: true });
-        });
-
-        const handles = await page.$$(
-          'button[id*="headlessui-listbox-button"]'
-        );
-        // 模拟鼠标移入
-        await Promise.all([
-          handles[0].hover(),
-          handles[0].click(),
-          new Promise((resolve) => setTimeout(resolve, 2000)),
-        ]);
-      }
-    } catch (err) {
-      console.error("抓取失败:", url, err);
-    }
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-  }
-  // browser.close(); // 永远不会到达这里，因为无限循环
-}
-
 async function startScraping() {
   try {
-    await Promise.all([fourMemePage(), b402scanAi()]);
+    await Promise.all([b402scanAi()]);
   } catch { }
 }
 
